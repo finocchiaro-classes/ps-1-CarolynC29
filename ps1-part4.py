654fcc9690fb35eaacce5b7d3cbc99e6b7f5bff8
CarolynC29 <chencaro@bc.edu>
2025-09-10 19:40:05 -0400

    Completed all 4 parts of the assignment Problem Set 1

--- ps1-part4.py
+++ ps1-part4.py
@@ -1,1 +1,18 @@
+prior = int(input("Prior arrests:"))
+priorLocal = input("Prior arrests for Local ordinance (Y/N):")
+age = int(input("Age at release:"))
 
+score = 0
+
+if prior >= 2:
+    score += 1
+if prior >= 5:
+    score +=1
+if priorLocal == "Y":
+    score += 1
+if age >= 18 and age <= 24:
+    score += 1
+if age >= 40:
+    score -= 1
+
+print(f"The recidivism risk score is {score}")
